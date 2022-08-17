d8352c5839671f6b80d6a93888b01fbf304276f3
endo1monir <94895713+andrewMonir@users.noreply.github.com>
2022-08-17 14:56:24 +0200

    all

---
+++ src/views/EventDetails.vue
@@ -0,0 +1,26 @@
+<template>
+<div v-if="event">
+<h1>{{event.title}}</h1>
+<p>{{event.time}} @ {{event.date}}</p>
+<p>{{event.description}}</p>
+</div>
+</template>
+<script>
+import EventService from '@/services/EventService'
+export default{
+    props:['id'],
+    data(){
+        return {
+            event:null,
+        }
+    },
+    created(){
+        //fetch api
+        EventService.getEvent(this.id).then(res =>{
+            this.event=res.data
+        }).catch(error=>{
+            console.log(error)
+        })
+    }
+}
+</script>
